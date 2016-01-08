c1f2f6c48e2abb3c692265ab47411c4492b14640
Kyle Kelley <rgbkrk@gmail.com>
2016-01-07 22:08:57 -0600

    Convert all the "multi-line" strings

--- src/index.js
+++ src/index.js
@@ -21,11 +21,38 @@ function readNotebook(filepath, options, callback) {
 
 export function readImmutableNotebook(filepath, options) {
   return new Promise((resolve, reject) => {
-    readNotebook(filepath, options, (err, nb) => {
+    readNotebook(filepath, options, (err, notebook) => {
       if (err) {
         reject(err);
       }
-      const immnb = Immutable.fromJS(nb);
+
+      // Convert the multiline strings from a raw v4 notebook so we have a nice
+      // consistent structure for Immutable.JS
+      for(const cell of notebook.cells) {
+        if(cell.source) {
+          if(Array.isArray(cell.source)) {
+            cell.source = cell.source.join('');
+          }
+        }
+        if(cell.outputs) {
+          for(const output of cell.outputs) {
+            if(output.output_type === 'stream') {
+              if(Array.isArray(output.text)) {
+                output.text = output.text.join('');
+              }
+            }
+            if (output.data) {
+              for (const mimetype in output.data) {
+                if (Array.isArray(output.data[mimetype])) {
+                  output.data[mimetype] = output.data[mimetype].join('');
+                }
+              }
+            }
+          }
+        }
+      }
+
+      const immnb = Immutable.fromJS(notebook);
       resolve(immnb);
     });
   });
