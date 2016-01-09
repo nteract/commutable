1ce979ab309f733672ddc47f0108f3c4ceabe740
Kyle Kelley <rgbkrk@gmail.com>
2016-01-08 23:39:34 -0600

    Helpful little REPL coloring.

--- scripts/repl.js
+++ scripts/repl.js
@@ -2,11 +2,22 @@ require.extensions['.ipynb'] = require.extensions['.json'];
 global.commutable = require('..');
 global.toyNotebook = require('../test/multiples.ipynb');
 
-console.log('console.log(toyNotebook)');
-console.log(global.toyNotebook);
+(() => {
+  const chalk = require('chalk');
+  const util = require('util');
 
-console.log('');
-console.log('console.log(commutable)');
-console.log(global.commutable);
+  // Quick helper functions
+  const codeLog = (s) => console.log(chalk.white(chalk.bgBlack(s)));
+  const insp = o => util.inspect(o, { colors: true });
+
+  codeLog('\nrequire.extensions[\'.ipynb\'] = require.extensions[\'.json\']');
+  codeLog('\nvar toyNotebook = require(\'../test/multiples.ipynb\')');
+  console.log(insp(global.toyNotebook));
+
+  codeLog('\nconsole.log(commutable)');
+  console.log(insp(global.commutable));
+
+  console.log(chalk.green('\nHINT: ') + chalk.white('var notebook = commutable.fromJS(toyNotebook)\n'));
+})();
 
 require('repl').start('> ');
