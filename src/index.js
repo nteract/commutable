e8a86c75893791cfce2641509b22802f05a60a5b
Kyle Kelley <rgbkrk@gmail.com>
2016-02-15 20:45:03 -0600

    Problem: We need easy ways to create new cells
    Solution: Provide helpers to create code and markdown cells

--- src/index.js
+++ src/index.js
@@ -46,3 +46,23 @@ export function Notebook(language_info) {
     language_info,
   });
 }
+
+export function createMarkdownCell() {
+  return fromJS({
+    'cell_type': 'markdown',
+    'metadata': {},
+    'source': '',
+  });
+}
+
+export function createCodeCell() {
+  return fromJS({
+    'cell_type': 'code',
+    'execution_count': null,
+    'metadata': {
+      'collapsed': false,
+    },
+    'source': '',
+    'outputs': [],
+  });
+}
