a3e4bd8d28a1b0968dc2b73e4d0cb3e15207b1f3
Jonathan Frederic <jon.freder@gmail.com>
2016-05-05 12:14:33 -0700

    Move newline to the back of split

--- src/cleaning.js
+++ src/cleaning.js
@@ -16,7 +16,7 @@ function cleanMultiline(item) {
  */
 function breakIntoMultiline(item) {
   // Use positive lookahead regex to split on newline and retain newline char
-  return typeof item === 'string' ? item.split(/(?=\n)/g) : item;
+  return typeof item === 'string' ? item.split(/(.+?\n)/g).filter(x=>x!=='') : item;
 }
 
 /**
