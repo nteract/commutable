e77d30371fbdfc12c277b51c14887e46f680ae8e
Safia Abdalla <safia@safia.rocks>
2016-05-21 12:38:07 -0500

    Add test for convert with error output

--- test/convert_spec.js
+++ test/convert_spec.js
@@ -17,3 +17,34 @@ describe('upgrade', () => {
     );
   });
 });
+
+describe('parse outputs', () => {
+  it('should read pyerror outputs correctly', () => {
+    const test = fromJS({
+      "metadata": {
+        "name": "test_error",
+      },
+      "nbformat": 3,
+      "nbformat_minor": 0,
+      "worksheets": [
+        {
+          "cells": [
+            {
+              "cell_type": "code",
+              "metadata": {},
+              "source": ["print('bound to error"],
+              "execution_count": 1,
+              "outputs": [{
+                "output_type": "pyerr",
+                "ename": "SyntaxError",
+                "evalue": "EOL while scanning string literal",
+              }]
+            },
+          ],
+        }
+      ]
+    });
+    const upgraded = upgrade(test, 3, 4);
+    expect(upgraded.getIn(['cells', 0, 'outputs', 0, 'output_type'])).to.equal("error");
+  });
+});
