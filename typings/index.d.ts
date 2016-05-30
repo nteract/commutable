8c470b208f090eee46616d6508c64908f91811e5
Safia Abdalla <safia@safia.rocks>
2016-05-30 17:32:20 -0500

    Update package.json with typings install

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/immutable/index.d.ts" />
-/// <reference path="modules/lodash.repeat/index.d.ts" />
-/// <reference path="modules/uuid/index.d.ts" />
--- typings/modules/lodash.repeat/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace lodash.repeat {
-    export function repeat(string?: string, n?: number): string;
-}
-
-export = lodash.repeat;
