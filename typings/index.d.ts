24dba99066642c37915ab1fc29d23cb7c2a3e8f3
Safia Abdalla <safia@safia.rocks>
2016-05-30 16:17:52 -0500

    Add node typings to solve error with undetected buffer module

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/immutable/index.d.ts" />
 /// <reference path="modules/lodash.repeat/index.d.ts" />
 /// <reference path="modules/uuid/index.d.ts" />
